ced19e4d8dda9b764836cbb76c2533c5a39e68df
LJCzzzzZ <1149387619@qq.com>
2024-09-29 12:33:30 +0800

    First Commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/LJCzzzzZ/Judger
+
+go 1.20
+
+require github.com/seccomp/libseccomp-golang v0.10.0
+
+require golang.org/x/sys v0.25.0 // indirect
---
+++ runner/util.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"syscall"
+	"unsafe"
+
+	libseccomp "github.com/seccomp/libseccomp-golang"
+)
+
+func FilterToBPF(filter *libseccomp.ScmpFilter) (*syscall.SockFprog, error) {
+	r, w, err := os.Pipe()
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		filter.ExportBPF(w)
+		filter.Release()
+		w.Close()
+	}()
+
+	bin, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &syscall.SockFprog{
+		Len: uint16(len(bin) / 8),
+		Filter: (*syscall.SockFilter)(unsafe.Pointer(&bin[0])),
+	}, nil
+}
